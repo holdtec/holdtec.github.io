424044ce2d55aa24addc6de28bd63528b5b01ef2
John <whitehatgo@gmail.com>
2024-10-30 01:58:35 +0800

    doc: some update

--- README.md
+++ README.md
@@ -1,45 +1,46 @@
-# 张三的个人主页
+# 个人主页
 
-欢迎来到我的个人主页！这里你可以找到我的开源项目、开发文档、用户指南以及后续计划的项目和资源。
+> 欢迎来到我的个人主页！这里你可以找到我的开源项目、开发文档、用户指南以及后续计划的项目和资源。
 
----
+### 🧑‍💻 关于作者
 
-## 🚀 项目列表
+- **John Wu, 一个独立开发者**
 
-| **当前项目**                                                 | **其他开源项目**                                             |
-| ------------------------------------------------------------ | ------------------------------------------------------------ |
-| - **[万能复式记账微信小程序](https://github.com/holdtec/holdtec.github.io)**  <br> 基于 Beancount 记账理念的多账本、多成员管理系统，内置会员管理和进销存功能。 | - **[开源项目1](https://github.com/your-repo1-url)**  <br> 项目描述简要说明。 |
-| - **[元初微信小程序SPA框架](https://github.com/your-repo-url)**  <br> 自研单页面应用框架，支持自定义路由、国际化和异步日志工具，简化微信小程序开发。 | - **[开源项目2](https://github.com/your-repo2-url)**  <br> 项目描述简要说明。 |
+- **擅长领域**：全栈开发、微信小程序开发、企业级财务管理系统构建
+- **首创并使用微信小程序单页面开发框架**完成了微信小程序*`万能复式记账`*
+- **个人 GitHub**：[点击查看](https://github.com/holdtec)
 
----
 
-## 📄 文档资源
 
-| **开发文档**                                                 | **用户文档**                                                 |
-| ------------------------------------------------------------ | ------------------------------------------------------------ |
-| - **[开发文档](https://your-domain.com/dev-docs)**  <br> 详细的 API 文档、系统架构、技术栈说明。 | - **[用户文档](https://your-domain.com/user-docs)**  <br> 为用户提供的操作指南和常见问题解答。 |
-| - **[更新日志](https://your-domain.com/changelog)**  <br> 项目各个版本的更新历史。 | - **[功能演示](https://your-domain.com/demo)**  <br> 实时功能演示与说明。 |
+### 🚀 当前项目
 
----
+ ![GitHub 仓库](https://img.shields.io/badge/GitHub-%23000000.svg?style=for-the-badge&logo=github&logoColor=white)
 
-## 🧑‍💻 关于我
+![GitHub stars](https://img.shields.io/github/stars/holdtec/holdtec.github.io?style=social)![GitHub forks](https://img.shields.io/github/forks/holdtec/holdtec.github.io?style=social) 
 
-- **姓名**：张三  
-- **公司**：元初信息技术有限公司  
-- **擅长领域**：全栈开发、微信小程序开发、企业级财务管理系统构建  
-- **个人 GitHub**：[holdtec](https://github.com/holdtec)
+**[万能复式记账微信小程序用户手册](https://holdtec.github.io)** `<br>` 基于 Beancount 记账理念的多账本、多成员管理系统，内置会员管理和进销存功能。
 
----
+### 📄 文档资源
 
-## 💡 后续计划
+| **开发文档**                                                 |
+| ------------------------------------------------------------ |
+| -**[用户文档](https://your-domain.com/user-docs)**  `<br/>` 为用户提供的操作指南和常见问题解答。 |
+| -**[功能演示](https://your-domain.com/demo)**  `<br/>` 实时功能演示与说明。 |
+| -**[开发文档](https://your-domain.com/dev-docs)**  `<br/>` 详细的 API 文档、系统架构、技术栈说明。 |
+| -**[更新日志](https://your-domain.com/changelog)**  `<br/>` 项目各个版本的更新历史。 |
+
+
+
+### 💡 后续计划
 
 - 增加新功能支持：自动化进销存管理模块
 - 扩展开源项目：创建更多实用的微信小程序框架
 - 完善当前项目中的会员管理和促销活动功能
+- **[开源元初微信小程序SPA框架](https://github.com/your-repo-url)**  `<br>` 自研单页面应用框架，支持自定义路由、国际化和异步日志工具，简化微信小程序开发。
 
----
 
-## 📧 联系方式
 
-- 如果你有任何问题或建议，请随时通过邮件与我联系：  
-  [support@your-domain.com](mailto:support@your-domain.com)
+### 📧 联系方式
+
+- 如果你有任何问题或建议，请随时通过邮件与我联系：
+  [发送邮件](mailto:whitehatgo@gmail.com)
--- book.js
+++ book.js
@@ -1,161 +1,159 @@
 //book.js
-module.exports={
-
-    root:".", //根目录
-    "language": "zh-hans",
-    "title" : "万能复式记账用户手册",
-    "author" : "John Woo",
-    "description" : "万能复式记账用户手册",
-    "theme-default": {
-        "showLevel": true
-    },
-    "label": {
-        "en": "Edit This Page",
-        "zh-cn": "编辑本页"
-    },
-
-    "styles": {
-      "website": "styles/website.css"
+module.exports = {
+  root: ".", //根目录
+  language: "zh-hans",
+  title: "万能复式记账用户手册",
+  author: "John Woo",
+  description: "万能复式记账用户手册",
+  "theme-default": {
+    showLevel: true,
   },
-    // 在左侧导航栏添加链接信息
-    "links" : {
-        "sidebar" : {
-            // "官网" : "https://www.wemarket.top",
+  label: {
+    en: "Edit This Page",
+    "zh-cn": "编辑本页",
+  },
 
-        }
+  styles: {
+    website: "styles/website.css",
+  },
+  // 在左侧导航栏添加链接信息
+  // links: {
+  //   sidebar: {
+  //     "官网" : "https://www.wemarket.top",
+  //   },
+  // },
+  "structure": {
+        "readme": "README.md"
     },
-    
-        // Plugins list, can contain "-name" for removing default plugins
-        "plugins": [
-            "-highlight",//替换官方插件
-            "prism", //代码着色
-            "prism-themes",
 
-            "github", //链接到github仓库
-            "sharing-plus",  //分享增强
-            "-lunr",
-            "-search",
-            "-sharing",
-            "search-pro", //增强中文搜索
-            "chapter-fold",//导航折叠插件
-            "splitter", //菜单动态伸缩.
-            "back-to-top-button",
-            "theme-fexa",
-            "code", //代码行号、复制
-            "tbfed-pagefooter",
-            "editlink",
-            "todo",
-            "codetabs",
-            "donate",
-            "theme-comscore", //多彩主题插件
-            "favicon", //网站的小图标
-            "versions-select" //多版本选择
-        ],
-        "variables": {
-            "themeFexa":{
-                "nav":[
-                    {
-                        "url":"https://www.wemarket.top",
-                        "target":"_self",
-                        "name": "官网首页"
-                    },
-                    {
-                        "url":"https://www.wemarket.top/news/",
-                        "target":"_self",
-                        "name": "博客文章"
-                    },
-                    {
-                        "url":"https://github.com/holdtec/holdtec.github.io.git",
-                        "target":"_blank",
-                        "name": "GitHub仓库"
-                    },
-                ]
-            },
+  // Plugins list, can contain "-name" for removing default plugins
+  plugins: [
+    "-highlight", //替换官方插件
+    "prism", //代码着色
+    "prism-themes",
+
+    "github", //链接到github仓库
+    "sharing-plus", //分享增强
+    "-lunr",
+    "-search",
+    "-sharing",
+    "search-pro", //增强中文搜索
+    "chapter-fold", //导航折叠插件
+    "splitter", //菜单动态伸缩.
+    "back-to-top-button",
+    "theme-fexa",
+    "code", //代码行号、复制
+    "tbfed-pagefooter",
+    "editlink",
+    "todo",
+    // "codetabs",
+    "donate",
+    "theme-comscore", //多彩主题插件
+    "favicon", //网站的小图标
+    "versions-select", //多版本选择
+  ],
+  variables: {
+    themeFexa: {
+      nav: [
+        {
+          url: "https://www.wemarket.top",
+          target: "_self",
+          name: "<strong>🏠官网首页</strong>",
         },
-
-        // Global configuration for plugins
-        "pluginsConfig": {
-            "pluginsConfig": {
-		"chapter-fold":{}
-	},
-            "theme-default": {
-                "showLevel": true
-            },
-            "github": {
-                "url": "https://github.com/holdtec/holdtec.github.io.git"
-            },
-            "prism": {
-                "css": [
-                    "prism-themes/themes/prism-gruvbox-light.css"
-                ]
-            },
-            "theme-fexa":{
-                "search-placeholder":"输入关键字搜索", //搜索框提示信息
-                "logo":"assets/logo-11.png", //logo地址
-                "favicon": "./favicon.ico" //ico地址
-            },
-            "tbfed-pagefooter": {
-                "copyright":"万能复式记账©️版权所有",
-                // "modify_label": "上次修改：",
-                "modify_format": "YYYY-MM-DD HH:mm:ss",
-            },
-            "sharing": {
-                // "douban": true,
-                // "facebook": true,
-                // "google": true,
-                // "twitter": true,
-                // "whatsapp": true,
-                // "all": [
-                //     "hatenaBookmark",
-                //     "instapaper",
-                //     "line",
-                //     "linkedin",
-                //     "messenger",
-                //     "pocket",
-                //     "qq",
-                //     "qzone",
-                //     "stumbleupon",
-                //     "viber",
-                //     "vk",
-                //     "weibo"
-                // ]
-            },
-            "editlink": {
-                "base": "https://github.com/holdtec/holdtec.github.io/blob/main",
-                "label": "Edit This Page",
-                "multilingual": false
-              },
-            "donate": {
-                "wechat": "/assets/wechat_qr.png",
-                "alipay": "/assets/alipay_qr.png",
-                "title": "请作者喝杯咖啡",
-                "button": "支持",
-                "alipayText": "",
-                "wechatText": ""
-            },
-            //多版本选择
-            // "versions":{
-            //     "options": [
-            //         {
-            //           "value": "",
-            //           "text": "Theme API"
-            //         },
-            //         {
-            //           "value": "",
-            //           "text": "Theme comscore",
-            //           "selected": true
-            //         }
-            //       ]
-            // },
-            "favicon": {
-                "shortcut": "favicon.ico",
-                "bookmark": "favicon.ico",
-                "appleTouch": "assets/logo_red.png",
-                "appleTouchMore": {
-                    "120x120": "assets/logo_red.png",
-                    "180x180": "assets/logo_red.png"
-                }
-            }
-
+        {
+          url: "https://www.wemarket.top/news/",
+          target: "_self",
+          name: "<strong>📄博客文章</strong>",
         },
-}
+        {
+          url: "https://github.com/holdtec/holdtec.github.io.git",
+          target: "_blank",
+          name: "<strong>🔗GitHub仓库</strong>",
+        },
+      ],
+    },
+  },
+
+  // Global configuration for plugins
+  pluginsConfig: {
+    pluginsConfig: {
+      "chapter-fold": {},
+    },
+    "theme-default": {
+      // showLevel: true,
+    },
+    github: {
+      url: "https://github.com/holdtec/holdtec.github.io.git",
+    },
+    prism: {
+      css: ["prism-themes/themes/prism-gruvbox-light.css"],
+    },
+    "theme-fexa": {
+      "search-placeholder": "输入关键字搜索", //搜索框提示信息
+      logo: "assets/logo-11.png", //logo地址
+      favicon: "./favicon.ico", //ico地址
+    },
+    "tbfed-pagefooter": {
+      copyright: "万能复式记账©️版权所有",
+      // "modify_label": "上次修改：",
+      modify_format: "YYYY-MM-DD HH:mm:ss",
+    },
+    sharing: {
+      // "douban": true,
+      // "facebook": true,
+      // "google": true,
+      // "twitter": true,
+      // "whatsapp": true,
+      // "all": [
+      //     "hatenaBookmark",
+      //     "instapaper",
+      //     "line",
+      //     "linkedin",
+      //     "messenger",
+      //     "pocket",
+      //     "qq",
+      //     "qzone",
+      //     "stumbleupon",
+      //     "viber",
+      //     "vk",
+      //     "weibo"
+      // ]
+    },
+    editlink: {
+      base: "https://github.com/holdtec/holdtec.github.io/blob/main",
+      label: "Edit This Page",
+      multilingual: false,
+    },
+    donate: {
+      wechat: "/assets/wechat_qr.png",
+      alipay: "/assets/alipay_qr.png",
+      title: "请作者喝杯咖啡",
+      button: "支持一下",
+      alipayText: "",
+      wechatText: "",
+    },
+    //多版本选择
+    // "versions":{
+    //     "options": [
+    //         {
+    //           "value": "",
+    //           "text": "Theme API"
+    //         },
+    //         {
+    //           "value": "",
+    //           "text": "Theme comscore",
+    //           "selected": true
+    //         }
+    //       ]
+    // },
+    favicon: {
+      shortcut: "favicon.ico",
+      bookmark: "favicon.ico",
+      appleTouch: "assets/logo_red.png",
+      appleTouchMore: {
+        "120x120": "assets/logo_red.png",
+        "180x180": "assets/logo_red.png",
+      },
+    },
+  },
+};
